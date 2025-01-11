e3b320206b4e6a9c32c007837109e639a9c24207
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-01-11 18:33:55 -0300

    feat: implement Disqus comments using Astro component

---
+++ src/components/Comments_old.tsx
@@ -0,0 +1,26 @@
+// import config from "../config.json";
+// import { DiscussionEmbed } from "disqus-react";
+// import React from "react";
+
+// interface DisqusConfig {
+//   id: string;
+//   url: string;
+//   title: string;
+// }
+
+// const Comments = ({id, url, title}: DisqusConfig) => {
+//   return (
+//         <DiscussionEmbed
+//           shortname='jmruiz'
+//           config={
+//             {
+//                 url: url,
+//                 identifier: id,
+//                 title: title
+//             }
+//           }
+//         />
+//   );
+// };
+
+// export default Comments;
